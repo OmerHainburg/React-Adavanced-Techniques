bd7f3ac40100e6c3592f7604728f0719f2593b73
Your Name <you@example.com>
2022-02-20 21:07:07 +0100

    Commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const h1Inline = <h1>Inline Styles</h1>  
+  const styleMe = <h2 style={{ background: 'lightblue', color: 'darkred' }}>Please style me! I am so bland!</h2>;
+  //
+  const h1ObjectVariable = <h1>Make A Style Object Variable</h1>  
+  const styles = {
+    background: 'blue',
+    color: 'darkred'
+  };
+  const stylesObject = <h2 style={styles}>Please style me! I am so bland!</h2>;
+  //
+  const h1StyleNameText = <h1>Style Name Syntax</h1>
+  const stylesNameText = {
+    background: 'lightblue',
+    color:      'darkred',
+    marginTop: '100px',
+    fontSize: '50px'
+  };
+  const h2StylesNameText = <h2 style={stylesNameText}>Please style me! I am so bland!</h2>;
+  //
+  const h1StyleValueSyntax = <h1>Style Value Syntax</h1>
+  // const stylesValueSyntax = {
+  //   background: 'lightblue',
+  //   color:      'darkred',
+  //   marginTop: 100px,
+  //   fontSize: 50px
+  // };
+  //
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {h1Inline}
+        {styleMe}
+        {h1ObjectVariable}
+        {stylesObject}
+        {h1StyleNameText}
+        {h2StylesNameText}
+        {h1StyleValueSyntax}
       </header>
     </div>
   );
